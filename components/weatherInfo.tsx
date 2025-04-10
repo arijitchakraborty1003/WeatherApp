489577a56d309132d39fc3e9a7d48c04d5d33d84
arijitchakraborty1003 <arijitchakraborty@rediffmail.com>
2025-04-11 00:18:36 +0530

    ADded Icons for weather
    Added different icons for different weather

--- components/weatherInfo.tsx
+++ components/weatherInfo.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import { WeatherData } from '../types';
 
-type Props = {
+interface Props {
   data: WeatherData;
-};
+}
 
 const WeatherInfo: React.FC<Props> = ({ data }) => {
+  const iconCode = data.weather[0].icon;
+  const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
+
   return (
     <View style={styles.container}>
       <Text style={styles.city}>{data.name}</Text>
-      <Text style={styles.temp}>{data.main.temp}°C</Text>
+      <Image source={{ uri: iconUrl }} style={styles.icon} />
+      <Text style={styles.temp}>{Math.round(data.main.temp)}°C</Text>
       <Text style={styles.desc}>{data.weather[0].description}</Text>
     </View>
   );
 };
 
-export default WeatherInfo;
-
 const styles = StyleSheet.create({
-  container: { marginTop: 20, alignItems: 'center' },
-  city: { fontSize: 24 },
-  temp: { fontSize: 40, fontWeight: 'bold' },
-  desc: { fontSize: 18, fontStyle: 'italic' },
+  container: { alignItems: 'center', marginTop: 20 },
+  city: { fontSize: 26, fontWeight: 'bold', marginBottom: 10 },
+  icon: { width: 100, height: 100 },
+  temp: { fontSize: 48, fontWeight: 'bold' },
+  desc: { fontSize: 20, textTransform: 'capitalize', marginTop: 10 },
 });
+
+export default WeatherInfo;
